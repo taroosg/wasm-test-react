7443410765d4b62cc4e001408f54fc268fbf9699
taroosg <root@taroosg.dev>
2021-12-17 16:50:49 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import init, { gen_rand_num } from '../wasm/pkg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    init()
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          {/* <button type="button" onClick={() => setCount((count) => count + 1)}> */}
+          <button type="button" onClick={() => gen_rand_num()}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ wasm/src/lib.rs
@@ -0,0 +1,27 @@
+mod utils;
+
+use rand::Rng;
+use wasm_bindgen::prelude::*;
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+  fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+  alert("Hello, wasm!");
+}
+
+#[wasm_bindgen]
+pub fn gen_rand_num() {
+  let mut rng = rand::thread_rng();
+
+  let n1: u8 = rng.gen();
+  alert(&format!("Random u8: {}", n1));
+}
